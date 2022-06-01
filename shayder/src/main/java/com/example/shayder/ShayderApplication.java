fabccc878a24971319e826c83eb1ff4ba387e8f3
Shayder Fontoura <shayderfontoura@gmail.com>
2022-06-01 17:09:24 -0300

    spring

---
+++ shayder/src/main/java/com/example/shayder/ShayderApplication.java
@@ -0,0 +1,13 @@
+package com.example.shayder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShayderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShayderApplication.class, args);
+	}
+
+}
---
+++ shayder/src/test/java/com/example/shayder/ShayderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shayder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShayderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
