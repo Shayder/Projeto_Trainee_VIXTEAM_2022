3d1d192438a4bdc66c6f3bab12a1c326f538ae0a
Shayder Fontoura <shayderfontoura@gmail.com>
2022-06-02 00:27:43 -0300

    Criação da classe 'HelloWord.java' e do html 'hello.html' em função de testes

---
+++ shayder/src/main/java/com/example/shayder/controller/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.example.shayder.controller;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class HelloWorld implements WebMvcConfigurer {
+    public void addViewControllers(ViewControllerRegistry registry){
+        registry.addViewController("/hello").setViewName("forward:/hello.html");
+    }
+}
